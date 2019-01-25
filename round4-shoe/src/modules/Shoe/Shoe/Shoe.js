942f5bee712cbb9ab5593e5ecad1edf2dfffe4d0
sylvie8029 <yawei8029@gmail.com>
2019-01-25 12:05:41 -0500

    CRUD all worked

--- round4-shoe/src/modules/Shoe/Shoe/Shoe.js
+++ round4-shoe/src/modules/Shoe/Shoe/Shoe.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteShoeStart, updateShoeStart } from '../store/ShoeActions';
 
 class Shoe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Shoe: props.Shoe
+      Shoe: props.Shoe,
+      isUpdating: false
     };
   }
+  onDeleteButtonClicked = () => {
+    this.props.deleteShoeStart(this.props.shoe.id);
+  };
+  onUpdateButtonClicked = () => {
+    this.setState({ isUpdating: true });
+  };
+  onSubmitButtonClicked = () => {
+    let newShoe = { shoeNumber: this.state.shoeNumber };
+    this.props.updateShoeStart(this.props.shoe.id, newShoe);
+    this.setState({ isUpdating: false });
+  };
+  onShoeNumberChanged = event => {
+    this.setState({ shoeNumber: event.target.value });
+  };
 
   render() {
     return (
       <div>
-        Shoe Works!
+        Shoe#:
         {this.props.shoe.shoeNumber}
+        {this.state.isUpdating ? (
+          <div>
+            <input
+              value={this.props.shoe.shoenumber}
+              onChange={this.onShoeNumberChanged}
+            />
+            <button onClick={this.onSubmitButtonClicked}>Submit</button>
+          </div>
+        ) : (
+          <button onClick={this.onUpdateButtonClicked}>update</button>
+        )}
+        <button onClick={this.onDeleteButtonClicked}>delete</button>
       </div>
     );
   }
@@ -22,7 +50,10 @@ class Shoe extends React.Component {
 const mapStateToProps = store => {
   return {};
 };
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  deleteShoeStart,
+  updateShoeStart
+};
 
 export default connect(
   mapStateToProps,
